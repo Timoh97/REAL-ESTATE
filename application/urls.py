8897909ab6e6b6bbd564f9c9f09c7b0dc80c2ff3
Timoh97 <timohmugendi@gmail.com>
2022-02-25 12:54:32 +0300

    change navbar

--- application/urls.py
+++ application/urls.py
@@ -12,9 +12,5 @@ path("agent/", views.agent, name="agent"),
 path("client/", views.client, name="client"),
 
 #authentication
-path('signup/', views.signup,name='signUp'),
-path('signup/donors/', views.donor_signup,name='customer_signup'),
-path('signup/NGO/', views.ngo_signup,name='author_signup'),
-path('login/',views.login_view, name = 'login_view'),   
-path('logout/', views.logout, name='logout'),
+
 ]
