ca996f51f9fb69db1a8264ebd3b1c841c91a5c18
Timoh97 <timohmugendi@gmail.com>
2022-03-03 14:54:13 +0300

    add password reset templates

--- project/urls.py
+++ project/urls.py
@@ -19,5 +19,7 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('application.urls')),
-    path('accounts/', include('allauth.urls')),
+    # path('accounts/', include('allauth.urls')),
+    #password reset urls
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
